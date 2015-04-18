04460b545bd548f603cb01d21e5ef531c595d87e
Alan Plotko <alan.plotko@gmail.com>
2015-04-18 16:18:44 -0400

    william, stop complaining

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Friendwagon' });
+});
+
+router.get('/roadtrip', function(req, res, next) {
+  res.render('view_roadtrip', { title: 'My Road Trip' });
 });
 
 module.exports = router;
