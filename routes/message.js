862c19b09bedf9589eba4fc46b7a0f042e97a968
William Jagels <will.jagels@gmail.com>
2015-04-18 21:29:55 -0400

    Proxy text

--- routes/message.js
+++ routes/message.js
@@ -3,8 +3,14 @@ var express = require('express');
 var router = express.Router();
 var client = require('twilio')(keys.TWILIO_SID, keys.TWILIO_TOKEN);
 
-router.get('/incoming', function(req, res, next) {
-    console.log(req.body);
+router.post('/incoming', function(req, res, next) {
+    client.messages.create({
+        from: keys.TWILIO_FROM,
+        body: req.body.body,
+        to: keys.TIWLIO_TO,
+    }, function(err, message) {
+        res.send(message.sid);
+    });
 });
 
 module.exports = router;
