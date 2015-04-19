e5de7d39bd62a2b4dcf2e99c475b7f8a3f96c396
William Jagels <will.jagels@gmail.com>
2015-04-18 22:46:52 -0400

    This might fix something

--- routes/message.js
+++ routes/message.js
@@ -5,10 +5,11 @@ var client = require('twilio')(keys.TWILIO_SID, keys.TWILIO_TOKEN);
 
 router.post('/incoming', function(req, res, next) {
     client.messages.create({
-        from: keys.TWILIO_FROM,
-        body: req.body.body,
         to: keys.TIWLIO_TO,
+        from: keys.TWILIO_FROM,
+        body: req.body.body
     }, function(err, message) {
+        console.log(err);
         res.send("hey");//message.sid);
     });
 });
