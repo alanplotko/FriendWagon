060cb14449676e094dbb9c0074183e3114e8400d
Gabe <gabe1118@yahoo.com>
2015-04-19 03:38:20 -0400

    Trips...

--- routes/trips.js
+++ routes/trips.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res, next) {
         });
     });
 
-    res.render('trips', {title : 'Users', clientID : keys.clientID});
+    //res.render('trips', {title : 'Users', clientID : keys.clientID});
 
 });
 
