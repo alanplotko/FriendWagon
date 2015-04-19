e7fca0ea94c769cf20d79b32bd63fecec2c277bb
Alan Plotko <alan.plotko@gmail.com>
2015-04-19 09:26:13 -0400

    Stuff

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,8 @@ router.get('/search', function(req, res, next) {
   res.render('search-form', { title: 'Search Road Trips' });
 });
 
+router.get('/roadtrip/:id', function(req, res, next) {
+  res.render('search-roadtrip', { title: 'Viewing Road Trip' });
+});
+
 module.exports = router;
